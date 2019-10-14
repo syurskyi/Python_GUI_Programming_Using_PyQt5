7fc3f014343fad134933061dda3d55cc247258da
Grey <sergejyurskyj@yahoo.com>
2019-10-14 15:18:58 -0700

    commit Section 6 82. Creating Layouts for Window

--- Section 6 Product Management App/Product Manager My/add_product.py
+++ Section 6 Product Management App/Product Manager My/add_product.py
@@ -7,6 +7,8 @@ from PyQt5.QtCore import Qt
 import sqlite3
 from PIL import Image
 
+connect = sqlite3.connect('products.db')
+cursor = connect.cursor
 
 class AddProduct(QWidget):
     def __init__(self):
@@ -19,10 +21,28 @@ class AddProduct(QWidget):
         self.show()
 
     def ui(self):
-        pass
+        self.widgets()
+        self.layouts()
 
     def widgets(self):
-        pass
+        # ##################widgets of top layout##########
+        self.add_product_image = QLabel()
+        self.image = QPixmap('icons/addproduct.png')
+        self.add_product_image.setPixmap(self.img)
+        self.title_text = QLabel("Add Product")
+        # ################widgets of bottom layot###########
+        self.name_entry = QLineEdit()
+        self.name_entry.setPlaceholderText("Enter name of product")
+        self.manufacturer_entry = QLineEdit()
+        self.manufacturer_entry.setPlaceholderText("Enter name of manufacturer")
+        self.price_entry = QLineEdit()
+        self.price_entry.setPlaceholderText("Enter price of product")
+        self.qouta_entry = QLineEdit()
+        self.qouta_entry.setPlaceholderText("Enter qouta of product")
+        self.upload_button = QPushButton("Upload")
+        # self.upload_button.clicked.connect(self.upload_image)
+        self.submitBtn = QPushButton("Submit")
+        self.submitBtn.clicked.connect(self.addProduct)
 
     def layouts(self):
         pass
