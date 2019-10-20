8ef7fa4d5c53760311e0d9b0dcfe13614290aa32
Grey <sergejyurskyj@yahoo.com>
2019-10-20 14:16:09 -0700

    commit Section 6 87. Creating Design of Add Member Window

--- Section 6 Product Management App/Product Manager My/add_member.py
+++ Section 6 Product Management App/Product Manager My/add_member.py
@@ -24,4 +24,46 @@ class AddMember(QWidget):
         self.show()
 
     def ui(self):
-        pass
+        # pass
+        self.widgets()
+        self.layouts()
+
+    def widgets(self):
+        # ############widgets of top layout###########
+        self.add_member_image = QLabel()
+        self.image = QPixmap('icons/addmember.png')
+        self.add_member_image.setPixmap(self.image)
+        self.add_member_image.setAlignment(Qt.AlignCenter)
+        self.title_text = QLabel("Add Member")
+        self.title_text.setAlignment(Qt.AlignCenter)
+
+        # # ###########widgets of bottom layout############
+        self.name_entry = QLineEdit()
+        self.name_entry.setPlaceholderText("Enter name of member")
+        self.surname_entry = QLineEdit()
+        self.surname_entry.setPlaceholderText("Enter surname of member")
+        self.phone_entry = QLineEdit()
+        self.phone_entry.setPlaceholderText("Enter phone number")
+        self.submit_btn = QPushButton("Submit")
+        # self.submit_btn.clicked.connect(self.add_member)
+
+    def layouts(self):
+        self.main_layout = QVBoxLayout()
+        self.top_layout = QVBoxLayout()
+        self.bottom_layout = QFormLayout()
+        self.top_frame = QFrame()
+        self.bottom_frame = QFrame()
+        # ############add widgets############
+        self.top_layout.addWidget(self.title_text)
+        self.top_layout.addWidget(self.add_member_image)
+        self.top_frame.setLayout(self.top_layout)
+        self.bottom_layout.addRow(QLabel("Name: "), self.name_entry)
+        self.bottom_layout.addRow(QLabel("SurName: "), self.surname_entry)
+        self.bottom_layout.addRow(QLabel("Phone: "), self.phone_entry)
+        self.bottom_layout.addRow(QLabel(""), self.submit_btn)
+        self.bottom_frame.setLayout(self.bottom_layout)
+
+        self.main_layout.addWidget(self.top_frame)
+        self.main_layout.addWidget(self.bottom_frame)
+
+        self.setLayout(self.main_layout)
